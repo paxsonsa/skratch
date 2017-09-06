467eae1100600431f728b143cbaa4ff01f121af8
Andrew Paxson <andrew@fx-mind.com>
2017-09-06 14:22:47 -0700

    Store Code Base

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='skratch',
+    version='1.0.1',
+    packages=['skratchlib'],
+    scripts=['scripts/skratch'],
+    url='github.com/paxsonsa/skratch',
+    license='MIT',
+    author='Andrew Paxson',
+    author_email='andrew.paxson@icloud.com',
+    description='Tool for creating, managing, and running skratch files'
+)
---
+++ skratchlib/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+""" 
+
+Author: Andrew Paxson
+Created: 2017-09-01
+
+"""
