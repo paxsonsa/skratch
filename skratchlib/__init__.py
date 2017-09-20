bded7414f5bc1beb80e8b3e234a5699cdb89273d
Andrew Paxson <andrew@fx-mind.com>
2017-09-19 20:51:23 -0700

    add run action, create function to get recently modified scratch files.
    TODO add functionality to list scratch files.

--- skratchlib/__init__.py
+++ skratchlib/__init__.py
@@ -6,9 +6,11 @@ Library Package for Skratch Tool
 Author: Andrew Paxson
 Created: 2017-09-01
 """
+#TODO Add List Scratch Files
 import argparse
 import os
 import re
+import subprocess
 
 SCRATCH_FILENAME_CONSTANT = "scratch"
 SCRATCH_LOCATION_CONSTANT = os.path.expanduser("~")
@@ -23,34 +25,44 @@ class CLIComponent(object):
     def action(self, args):
         raise NotImplementedError("action method needs to be implemented on {0}".format(self.__class__.__name__))
 
+
 class NewScratchDelegate(CLIComponent):
     def action(self, args):
         """ Create New Scratch File """
         create_new_scratch_file(args.file, self.settings, py_template_func)
 
-    def add_cli(self, action):
+    def add_cli(self, subparser):
         """ Creates New Scratch CLI Interface on the given argparse.subparser
 
         Args:
-            action (argparse._SubParsersAction): action to add arguments too
+            subparser (argparse._SubParsersAction): action to add arguments too
 
         Returns:
             argparse._SubParsersAction
         """
-        new_parser = action.add_parser('create', help='create new scratch file')
+        new_parser = subparser.add_parser('create', help='create new scratch file')
         new_parser.add_argument('name', nargs='?', default=None, help="Optional Name to be given to the file, "
                                                                       "default name  is an increment of 'scratch##'")
         new_parser.set_defaults(func=self.action)
-        return action
+        return subparser
 
 
 class RunScratchDelegate(CLIComponent):
 
-    def add_cli(self, action):
-        new_parser = action.add_parser('run', help='runscratch file')
-        new_parser.add_argument('file', nargs='?', default=None, help="Optional scratch filename to run.")
+    def action(self, args):
+        file_ = args.file
+        if not file_:
+            file_ = get_recently_modified_scratch_file(self.settings)
+        if not file_:
+            raise RuntimeError("could not location files to run!")
+        run_scratch_file(file_, self.settings)
+
+    def add_cli(self, subparser):
+        new_parser = subparser.add_parser('run', help='runscratch file')
+        new_parser.add_argument('file', nargs='?', default="", help="Optional scratch filename to run, if not given "
+                                                                    "it will run skratch with the latest mtime")
         new_parser.set_defaults(func=self.action)
-        return action
+        return subparser
 
 
 class SettingData(object):
@@ -92,7 +104,7 @@ def _open_with_editor(path, editor):
 
 
 def py_template_func(settings):
-    return "#! /usr/bin/python"
+    return "#! /usr/bin/env python\n"
 
 
 def create_new_scratch_file(file_name, settings, template_func=py_template_func):
@@ -121,6 +133,29 @@ def create_new_scratch_file(file_name, settings, template_func=py_template_func)
     _open_with_editor(path, SCRATCH_DEFAULT_EDITOR)
     return path
     # paths = map(lambda x: os.path.join(settings.scratch_location, x), scratch_files)
+
+
+def get_recently_modified_scratch_file(settings):
+    """ Returns the most recently modified scratch in skratch dir
+
+    Args:
+        settings (SettingData): settings to use for getting scratch dir
+
+    Returns:
+        filepath (str) or empty string is nothing was found
+    """
+    dir_contents = os.listdir(settings.location)
+    full_paths = map(lambda f: os.path.join(settings.location, f), dir_contents)
+    files = filter(lambda  f: os.path.isfile(str(f)), full_paths)
+    if not files:
+        return ""
+    files = sorted(files, key=_get_mtime)
+    return files[-1]
+
+
+def run_scratch_file(file_name, settings):
+    """ Runs Given Scratch Filename with settings """
+    return subprocess.call([file_name])
 
 
 def cli_parser(settings):
--- tests/test_skratch_lib.py
+++ tests/test_skratch_lib.py
@@ -4,9 +4,39 @@
 Author: Andrew Paxson
 Created: 2017-09-12
 """
+import os
 import pytest
 
 import skratchlib
+
+TEST_LOCATION = os.path.join(os.getcwd(), "testdir")
+
+
+@pytest.fixture()
+def test_files():
+
+    os.mkdir(TEST_LOCATION)
+
+    scratch_files = [
+        "scratch1", "flirt", "THISONE"
+    ]
+    for f in scratch_files:
+        with open(os.path.join(TEST_LOCATION, f), 'w') as fh:
+            fh.write("Hello, {0}".format(f))
+
+    yield TEST_LOCATION, scratch_files
+
+    print("Cleaning Up...")
+    for f in scratch_files:
+        os.remove(os.path.join(TEST_LOCATION, f))
+    os.removedirs(TEST_LOCATION)
+
+
+@pytest.fixture()
+def location():
+    os.mkdir(TEST_LOCATION)
+    yield TEST_LOCATION
+    os.removedirs(TEST_LOCATION)
 
 
 @pytest.mark.parametrize("files, expected", (
@@ -35,8 +65,30 @@ def test_create_new_scratch_file():
     path = skratchlib.create_new_scratch_file("testfile", settings)
     assert os.path.basename(path) == "testfile"
     assert os.path.exists(path)
-    os.remove(path)
+    os.remove(path)     # Clean Up
 
+
+def test_get_most_recently_modified_scratch_empty(location):
+    settings = skratchlib.SettingData()
+    settings.location = location
+    settings.editor = "echo"
+
+    # Test Return "" when no files exist
+    fp = skratchlib.get_recently_modified_scratch_file(settings)
+    assert fp == ""
+
+
+def test_get_most_recently_modified_scratch(test_files):
+    # Settings
+    settings = skratchlib.SettingData()
+    settings.location = test_files[0]
+
+    latest = os.path.join(settings.location, test_files[1][-1])
+
+    fp = skratchlib.get_recently_modified_scratch_file(settings)
+    assert fp == latest
+
+# TODO Create Actions Tests for CLI Component
 
 def test_cli_parser():
     settings = skratchlib.SettingData()
